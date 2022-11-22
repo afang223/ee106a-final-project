c1272539aca5e8f3ebb733b90de638229d9a6dbd
Class Account <ee106a-ahc@c105-8.local>
2022-11-21 19:37:32 -0800

    wooo start

---
+++ build/ik/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cc/ee106a/fa22/class/ee106a-ahc/ros_workspaces/final_project/src/ik
+# Build directory: /home/cc/ee106a/fa22/class/ee106a-ahc/ros_workspaces/final_project/build/ik
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
